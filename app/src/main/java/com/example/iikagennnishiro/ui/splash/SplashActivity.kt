e7f1c17d4f613d22f8445b5735d260ab45d37ce0
tomaspoppo <tomaspoppo@hotmail.co.jp>
2025-02-16 18:34:49 +0900

    スプラッシュ画面成功画面内にロゴが収まった！

--- app/src/main/java/com/example/iikagennnishiro/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/example/iikagennnishiro/ui/splash/SplashActivity.kt
@@ -6,16 +6,20 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import com.example.iikagennnishiro.MainActivity
+import com.example.iikagennnishiro.R
 
 class SplashActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // スプラッシュ画面のレイアウトを適用
+        setContentView(R.layout.activity_splash)
 
         // 2秒後に MainActivity へ遷移
         Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish() // スプラッシュ画面を閉じる
-        }, 3300) // 3300ミリ秒 (2秒)
+        }, 3300) // 2000ミリ秒 (2秒)
     }
 }
