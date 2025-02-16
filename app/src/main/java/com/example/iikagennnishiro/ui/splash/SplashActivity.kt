7fa26a7ff69b3f8d5604e775e5203b5935cf8c5f
tomaspoppo <tomaspoppo@hotmail.co.jp>
2025-02-16 17:51:24 +0900

    スプラッシュ画面成功だけどロゴがはみ出る

---
+++ app/src/main/java/com/example/iikagennnishiro/ui/splash/SplashActivity.kt
@@ -0,0 +1,21 @@
+package com.example.iikagennnishiro.ui.splash
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.example.iikagennnishiro.MainActivity
+
+class SplashActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // 2秒後に MainActivity へ遷移
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish() // スプラッシュ画面を閉じる
+        }, 2000) // 2000ミリ秒 (2秒)
+    }
+}
